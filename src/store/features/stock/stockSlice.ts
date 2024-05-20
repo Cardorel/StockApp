c9ac1bc7f0ff0238151bc188028f08ff7dbbdb75
NgassakiJunescyPRATH <junescy.ngassaki@pratho.com>
2024-05-20 19:58:52 +0200

    changed the url link

--- src/store/features/stock/stockSlice.ts
+++ src/store/features/stock/stockSlice.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 export const stockSlice = createApi({
   reducerPath: 'stock',
-  baseQuery: fetchBaseQuery({baseUrl: process.env.API_URL}),
+  baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com/'}),
   endpoints: builder => ({
     getListOfStocks: builder.query({
       query: (search: string) => {
